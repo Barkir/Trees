9970e6eb7cdb1bf5814758f704dd6a37911d3d1e
Barkir <r.barinov2006@gmail.com>
2025-10-25 17:33:36 +0300

    Added CMake added insert function to trees

--- include/tree.hpp
+++ include/tree.hpp
@@ -1,30 +1,35 @@
 
-template<typename KeyT, typename T>
+template<typename KeyT>
 struct Node {
     public:
-        Node *left   = nullptr;
-        Node *right  = nullptr;
-        Node *parent = nullptr;
+        Node<KeyT> *left   = nullptr;
+        Node<KeyT> *right  = nullptr;
+        Node<KeyT> *parent = nullptr;
         int height = 0;
-        T key_;
+        KeyT key_;
 
     public: // constructor
         Node(KeyT key) : key_(key) {}
+
+    public:
+        void setParent(Node *node) {
+            parent = node;
+        }
 
 };
 
 template <typename KeyT, typename Comp>
 class AVLTree {
     private:
-        Node *top_node;
+        Node<KeyT> *top_node;
 
     public:
         void insert(KeyT key);
-        Node* lower_bound(KeyT key);
-        Node* upper_bound(KeyT key);
+        Node<KeyT>* lower_bound(KeyT key);
+        Node<KeyT>* upper_bound(KeyT key);
 
     public: // dump
-        void dumpTree(Node *node);
+        void dumpTree(Node<KeyT> *node);
 
     public: // constructor
         AVLTree() {
@@ -34,22 +39,24 @@ class AVLTree {
 template <typename KeyT, typename Comp>
 void AVLTree<KeyT, Comp>::insert(KeyT key) {
     Node n = Node(key);
-    Node *changable = top_node;
+    Node<KeyT> *changable = top_node;
     while (changable->left || changable->right) {
-        if (Comp(n, *changable) > 0) {
+        if (Comp(n.key_, (*changable).key_) > 0) {
             if (changable->right) {
                 changable = changable->right;
+                n.height++;
             } else {
                 changable->right = &n;
-                n.parent = changable;
+                n.setParent(changable); // use setParent instead
                 break;
             }
         } else {
             if (changable->right) {
                 changable = changable->left;
+                n.height++;
             } else {
                 changable->left = &n;
-                n.parent = changable;
+                n.setParent(changable); // use setParent instead
                 break;
             }
         }
@@ -57,10 +64,10 @@ void AVLTree<KeyT, Comp>::insert(KeyT key) {
 
     // balance_tree();
 }
-
-template <typename KeyT, typename Comp>
-void AVLTree<KeyT, Comp>::dumpTree(Node *node) {
-    if (node->)
-}
+//
+// template <typename KeyT, typename Comp>
+// void AVLTree<KeyT, Comp>::dumpTree(Node *node) {
+//     if (node->)
+// }
 
 
--- src/tree.cpp
+++ src/tree.cpp
@@ -1,1 +1,17 @@
 #include <iostream>
+#include <functional>
+
+#include "tree.hpp"
+#include "CLI11.hpp"
+
+int compareInt(int a, int b) {
+    return a > b;
+}
+
+using funcIt = std::function<int(int, int)>;
+
+int main() {
+    AVLTree<int, funcIt> tree;
+    tree.insert(1);
+
+}
